9b3ce088720fa7a9cd22844b40696bbf90888065
Hossahin <hossahin321@gmail.com>
2025-10-30 00:54:01 +0600

    chore: create Next.js project for job task

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LeftSidebar from "@/components/LeftSidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <LeftSidebar />
+
+        <div className="border">{children}</div>
       </body>
     </html>
   );
